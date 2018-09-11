820a7f8a6100e93ac86b1e273ecd066a7bbaea57
James <jimmyskripchuk@gmail.com>
2018-09-11 18:00:15 -0400

    test

--- CISC361/Proj1b/mycp.c
+++ CISC361/Proj1b/mycp.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 
-#define	BUFFSIZE	10
+#define	BUFFSIZE	100
 
 //Going to have to do multiple system calls since buffer will get full with big files
 //While loop?
@@ -14,13 +15,24 @@
 
 int main(int argc, char** argv){
     char buf[BUFFSIZE];
+    int len;
 
    // buf = "Hello!";
 
     int file1 = open("testfile.txt", O_RDONLY);
+    int file2 = open("testfile2.txt", O_WRONLY | O_CREAT);
+    
     read(file1, buf, BUFFSIZE);
-    printf("%d\n", file1);
-    printf("%s",buf);
+    
+    len = (int)strlen(buf);
+    buf[len-1] = '\0';
+    char str[len];
+    strcpy(str, buf);
+    //printf("%d\n", file1);
+    //printf("%s",str);
+    write(file2, buf, BUFFSIZE);
+    close(file1);
+    close(file2);
     //sprintf(buf, )
     //printf("Hello world!");
     return 0;
