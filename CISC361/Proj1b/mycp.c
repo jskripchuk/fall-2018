01b99d4ffa4b69da16a38bb90cbc187874b8ed33
James Skripchuk <jimmyskripchuk@gmail.com>
2018-09-14 11:31:59 -0400

    COOODE

--- CISC361/Proj1b/mycp.c
+++ CISC361/Proj1b/mycp.c
@@ -1,39 +1,82 @@
+/*
+James Skripchuk
+CISC361
+Project 1b: mycp
+*/
 
-#include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
 
-#define	BUFFSIZE	100
-
-//Going to have to do multiple system calls since buffer will get full with big files
-//While loop?
-
-//read call keeps a pointer and stuff so it's nice
-//multiple reads iterate though the file
-
+#define	BUFFSIZE 2048
 
 int main(int argc, char** argv){
+    //A temporary char buffer for reading/writing
     char buf[BUFFSIZE];
-    int len;
 
-   // buf = "Hello!";
+    //Various strings to print when errors/messages happen
+    char ARG_ERROR[] = "Incorrect amount of arguments!\n";
+    char SOURCE_ERROR[] = "Source file does not exist!\n";
+    char PERMISSIONS_ERROR[] = "Insufficent permissions for source file!\n";
+    char WRITE_ERROR[] = "Error writing to file! \n";
+    char EXISTS_MESSAGE[] = "Destination file already exists. Overwrite? [Y/N]: \n";
 
-    int file1 = open("testfile.txt", O_RDONLY);
-    int file2 = open("testfile2.txt", O_WRONLY | O_CREAT);
-    
-    read(file1, buf, BUFFSIZE);
-    
-    len = (int)strlen(buf);
-    buf[len-1] = '\0';
-    char str[len];
-    strcpy(str, buf);
-    //printf("%d\n", file1);
-    //printf("%s",str);
-    write(file2, buf, BUFFSIZE);
-    close(file1);
-    close(file2);
-    //sprintf(buf, )
-    //printf("Hello world!");
+
+    //mycp must take 3 arguments
+    if(argc != 3){
+        write(STDERR_FILENO, ARG_ERROR, strlen(ARG_ERROR));
+        return -1;
+    }
+
+    //Check if the source file exists 
+    if(access(argv[1], F_OK) != 0){
+        write(STDERR_FILENO, SOURCE_ERROR, strlen(SOURCE_ERROR));
+        return -1;
+    //Check if we have permissions to read the source file
+    }else if(access(argv[1], R_OK) != 0){
+        write(STDERR_FILENO, PERMISSIONS_ERROR, strlen(PERMISSIONS_ERROR));
+        return -1;
+    }
+
+    //If destination file already exists, prompt user if they want to overwrite
+    if(access(argv[2], F_OK) == 0){
+        write(STDOUT_FILENO, EXISTS_MESSAGE, strlen(EXISTS_MESSAGE));
+        read(STDIN_FILENO, buf, BUFFSIZE);
+
+        int len;
+        len = (int)strlen(buf);
+        buf[len-1]='\0';
+
+        //Loops until a valid input is reached
+        while(strcmp(buf, "Y") != 0 && strcmp(buf, "N") != 0){
+            write(STDOUT_FILENO, EXISTS_MESSAGE, strlen(EXISTS_MESSAGE));
+            read(STDIN_FILENO, buf, BUFFSIZE);
+            len = (int)strlen(buf);
+            buf[len-1]='\0';
+        }
+
+        //Exit if N is selected
+        if(strcmp(buf, "N") == 0){
+          return 0;
+        }
+    }
+
+    //Open files
+    int source = open(argv[1], O_RDONLY);
+    int dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC);
+
+    int n;
+    //Write in data from source file into destination file
+    while ((n = read(source, buf, BUFFSIZE)) > 0){
+		if (write(dest, buf, n) != n){
+            write(STDERR_FILENO, WRITE_ERROR, strlen(WRITE_ERROR));
+            return -1;
+        }
+    }
+
+    //Close up files
+    close(source);
+    close(dest);
+
     return 0;
 }
